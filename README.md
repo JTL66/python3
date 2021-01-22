eb3c1848ec35def4ba2d6d1f2d2a82a488382956
JTL66 <65905010+JTL66@users.noreply.github.com>
2021-01-22 15:59:47 -0500

    commits

--- README.md
+++ README.md
@@ -14,11 +14,26 @@ $ jupyter notebook
 ## Getting Started
 - Table of Contents
 - Hello World
+  [x] [`lect1.ipynb`](https://github.com/JTL66/python3/blob/main/lec1.ipynb)
     - Welcome
     - Comments
     - Helloworld
+    - Strings
+    - Variables
+    - Errors
+    - Numbers
+    - Calculations
+    - Changing Numbers
+    - Exponents
+    - Modulo
+    - Concatenation
+    - Plus Equals
+    - Multi-line Strings
+    - Block Letters
+    [x] [`lect1.ipynb`](https://github.com/JTL66/python3/blob/main/lec1.ipynb)
     - 
-    - 
+
+
 - Control Flow
     - 
 - Functions
---
+++ initials.py
@@ -0,0 +1,57 @@
+## BLock Letters
+# initials
+
+# Block Letters
+print(" SSS""   L") 
+print ("S""   S""  L")
+print ("S""      L")
+print(" SSS""   L")
+print("    S""  L")
+print ("S""   S""  L")
+print(" SSS""   LLLLLL") 
+
+
+# Sonny Li
+# Fun Fact: I played guitar in a band called Attica.
+
+print("  SSS  ")
+print(" S   S ")
+print(" S     ")
+print("  SSS  ")
+print("     S ")
+print(" S   S ")
+print("  SSS  ")
+
+
+name = """ 
+  SSS
+ S   S 
+ S     
+  SSS 
+     S
+ S   S
+  SSS  
+"""
+ 
+print(name)
+
+print("L") 
+print("L")
+print("L")
+print("L")
+print("L")
+print("L")
+print("LLLLLL") 
+
+
+name = """
+L
+L
+L
+L
+L
+L
+LLLLLL
+"""
+
+print(name)
